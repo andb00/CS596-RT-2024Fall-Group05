ef42d42993e0fb5f498dc9a6eea2ce6bb6377d51
andb00 <andrewbetancourt00@gmail.com>
2024-10-17 18:36:16 -0700

    User calculator app

---
+++ proj2/apps/test_calc/test_calc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sys_calc.c"
+
+int main(int argc, const char * arg[]) {
+    int *result;
+    sys_calc(atoi(argv[0], atoi(argv[2], argv[1], &result);
+    return 0;
+}
